ece27c165dfaa067f74a658a8c30243c8f5474b7
Nelio <equipe.kipedido@gmail.com>
2025-10-01 17:33:40 -0300

    Primeiro commit do projeto completo

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+var krms_config ={			
+	'ApiUrl':"https://www.newdelivery.com.br/mobileappv2/api",		
+	'AppTitle':"New Delivery",
+	'ApiKey' : '990383newdelivery990383',
+	'LicenseKey': '27ed4e855d34a3403de78e8edc911d5c',
+	'debug': true
+};
